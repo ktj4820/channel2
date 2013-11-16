f92e8a9e60e879d56dd0b863a77dc7f980d8de89
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2013-11-16 09:14:54 -0500

    added nav menu highlighting

---
+++ channel2/core/templatetags/core_tags.py
@@ -0,0 +1,37 @@
+from django import template
+from django.http.request import QueryDict
+
+register = template.Library()
+
+
+@register.filter
+def getparams(value, exclude=None):
+    """
+    getparams returns the url get string for value (instance of QueryDict). E.g.
+
+        &p=1&authors=test-author
+
+    exclude should be comma separated names of parameters to exclude from
+    urlencode. E.g.
+
+        request.GET|getparams:"p"
+        request.GET|getparams:"p,q"
+    """
+
+    if not isinstance(value, QueryDict):
+        raise template.TemplateSyntaxError("getparams should be used on QueryDict only (e.g. request.GET)")
+    if exclude:
+        value = value.copy()
+        for key in exclude.split(','):
+            if key in value: del value[key]
+    return value.urlencode()
+
+
+@register.filter
+def startswith(value, s):
+    """
+    returns if value starts with s, used for menu highlighting
+    """
+
+    if not value: return False
+    return value.find(s) == 0
