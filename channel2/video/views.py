74e3fd0368d3e03cc1cdf62e6d51ec0402ca8d98
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2013-11-16 10:40:48 -0500

    rendering contents for video list

--- channel2/video/views.py
+++ channel2/video/views.py
@@ -1,9 +1,16 @@
+from channel2.core.utils import paginate
 from channel2.core.views import ProtectedTemplateView
+from channel2.video.models import Video
 
 
 class VideoListView(ProtectedTemplateView):
 
     template_name = 'video/video-list.html'
+    page_size = 12
 
     def get(self, request):
-        return self.render_to_response({})
+        video_list = Video.objects.order_by('-created_on')
+        video_list = paginate(video_list, self.page_size, request.GET.get('p'))
+        return self.render_to_response({
+            'video_list': video_list,
+        })
