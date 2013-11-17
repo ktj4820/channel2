8dd0c5d8ec6f05f427002c9338da3dc003ae0f65
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2013-11-17 14:40:38 -0500

    upload video files and covers into folders that are named after labels

--- channel2/video/models.py
+++ channel2/video/models.py
@@ -11,12 +11,14 @@ from channel2.settings import FFMPEG_PATH
 
 def video_file_upload_to(instance, filename):
     filename = '{}.mp4'.format(instance.slug)
-    return os.sep.join(['video', filename])
+    dir = instance.label.slug if instance.label else 'unknown'
+    return os.sep.join(['video', dir, filename])
 
 
 def video_cover_upload_to(instance, filename):
     filename = '{}.jpg'.format(instance.slug)
-    return os.sep.join(['cover', filename])
+    dir = instance.label.slug if instance.label else 'unknown'
+    return os.sep.join(['cover', dir, filename])
 
 
 class Video(models.Model):
