046c736941a44eb9daf801171654e23bbf47ae1b
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-05-10 14:06:58 -0400

    CH-2 - Added localsettings.py for travis

---
+++ config/travis/localsettings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'channel2',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'TEST_NAME': 'channel2_test',
+    }
+}
