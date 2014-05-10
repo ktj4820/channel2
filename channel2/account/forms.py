e82acab5b488e681b5916662b458b03d0600ecaf
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-05-10 17:11:32 -0400

    CH-5 - Added basic styles for login page

---
+++ channel2/account/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.forms.widgets import EmailInput
+from django.utils.translation import ugettext_lazy as _
+
+
+class AccountLoginForm(forms.Form):
+
+    email = forms.EmailField(
+        label=_('Email'),
+        widget=EmailInput(attrs={
+            'autocomplete': 'off',
+            'class': 'account-input',
+            'placeholder': 'Email',
+            'required': 'required',
+        })
+    )
+    password = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput(attrs={
+            'class': 'account-input',
+            'placeholder': 'Password',
+            'required': 'required',
+        })
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_cache = None
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = authenticate(email=email, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError('Please enter a correct email and password. Note that both fields are case-sensitive.')
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user_cache
--- channel2/account/views.py
+++ channel2/account/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import logout, login
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from channel2.account.forms import AccountLoginForm
 from channel2.core.views import TemplateView
 
 
@@ -5,8 +9,22 @@ class AccountLoginView(TemplateView):
 
     template_name = 'account/account-login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect('video.list')
+        return super(AccountLoginView, self).dispatch(request, *args, **kwargs)
+
     def get(self, request):
-        return self.render_to_response({})
+        return self.render_to_response({
+            'form': AccountLoginForm()
+        })
 
     def post(self, request):
-        return self.render_to_response({})
+        form = AccountLoginForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            next_url = request.POST.get('next', reverse('video.list'))
+            return redirect(next_url)
+        return self.render_to_response({
+            'form': form,
+        })
