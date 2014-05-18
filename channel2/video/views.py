f1bab4f4f53b3f0d4f7f66b12c695f4e2f9c19ae
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-05-18 01:59:30 -0400

    CH-9 - Refactored out video and tag list components, also updated styles

--- channel2/video/views.py
+++ channel2/video/views.py
@@ -9,7 +9,7 @@ class VideoListView(ProtectedTemplateView):
     page_size = 20
 
     def get(self, request):
-        video_list = Video.objects.order_by('-created_on')
+        video_list = Video.objects.order_by('-created_on').select_related('tag').prefetch_related('tag__children')
         video_list = paginate(video_list, self.page_size, request.GET.get('p'))
         return self.render_to_response({
             'video_list': video_list,
