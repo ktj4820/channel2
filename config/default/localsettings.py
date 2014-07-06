546dccb2f54a3aa3a30a640fd9423972e8a9c194
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-07-06 02:38:02 -0400

    CH-24 - Added application level logging

--- config/default/localsettings.py
+++ config/default/localsettings.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 DEBUG = False
 DEBUG_TOOLBAR = False
@@ -37,4 +38,53 @@ EMAIL_USE_TLS = True
 
 FFMPEG_PATH = '/usr/local/bin/ffmpeg'
 VIDEO_DIR = '/var/www/channel2/downloads/'
-X_ACCEL = False
+X_ACCEL = True
+
+#-------------------------------------------------------------------------------
+# logging settings
+#-------------------------------------------------------------------------------
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    # formatters
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+
+    # filters
+    'filters': {},
+
+    # handlers
+    'handlers': {
+        'email_admin': {
+            'level': 'ERROR',
+            'filters': [],
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+        'channel2': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': '/var/www/channel2/logs/channel2-{}.log'.format(datetime.datetime.now().strftime('%Y%m%d')),
+            'encoding': 'utf-8',
+        },
+    },
+
+    # loggers
+    'loggers': {
+        '': {
+            'handlers': ['channel2'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['email_admin', 'channel2'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
