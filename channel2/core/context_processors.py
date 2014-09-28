5d885c8fafa56b553af1882545250566e88d5ed1
derekkwok <derek.kai.chun.kwok@gmail.com>
2014-09-28 00:16:50 -0400

    CH-59 - Added header and nav styles

--- channel2/core/context_processors.py
+++ channel2/core/context_processors.py
@@ -1,7 +1,12 @@
+import hashlib
+
 from channel2.settings import RESOURCE_VERSION
 
 
 def resource_version(request):
-    return {
+    context = {
         'RESOURCE_VERSION': RESOURCE_VERSION,
     }
+    if request.user.is_authenticated():
+        context['GRAVATAR_HASH'] = hashlib.md5(request.user.email.encode('utf-8')).hexdigest()
+    return context
