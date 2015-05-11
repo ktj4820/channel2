ead27a37fa317d352970032796d0e327bddd1ae9
derekkwok <derek.kai.chun.kwok@gmail.com>
2015-05-11 18:35:00 -0400

    CH-3 - Finished styles for home page

--- channel2/home/views.py
+++ channel2/home/views.py
@@ -1,4 +1,6 @@
 from channel2.core.views import ProtectedTemplateView
+from channel2.tag.enums import TagType
+from channel2.tag.models import Tag
 
 
 class HomeView(ProtectedTemplateView):
@@ -6,4 +8,9 @@ class HomeView(ProtectedTemplateView):
     template_name = 'home/home.html'
 
     def get(self, request):
-        return self.render_to_response({})
+        new_list = Tag.objects.filter(type=TagType.ANIME).prefetch_related('children').order_by('-created_on')[:10]
+        for tag in new_list:
+            tag.children_list = sorted(tag.children.all(), key=lambda t: t.name)
+        return self.render_to_response({
+            'new_list': new_list,
+        })
--- channel2/tag/views.py
+++ channel2/tag/views.py
@@ -11,9 +11,12 @@ class TagListView(ProtectedTemplateView):
 
     template_name = 'tag/tag-list.html'
 
+    def get_tag_list(self):
+        return Tag.objects.all()
+
     def get(self, request):
         tag_dict = defaultdict(list)
-        for tag in Tag.objects.all():
+        for tag in self.get_tag_list():
             k = tag.name[0].upper()
             if k.isdigit(): k = '#'
             tag_dict[k].append(tag)
@@ -26,15 +29,12 @@ class TagListView(ProtectedTemplateView):
         })
 
 
-class TagListAnimeView(ProtectedTemplateView):
+class TagListAnimeView(TagListView):
 
-    template_name = 'tag/tag-list-anime.html'
+    template_name = 'tag/tag-list.html'
 
-    def get(self, request):
-        tag_list = Tag.objects.filter(type=TagType.ANIME).order_by('name')
-        return self.render_to_response({
-            'tag_list': tag_list,
-        })
+    def get_tag_list(self):
+        return Tag.objects.filter(type=TagType.ANIME)
 
 
 class TagView(ProtectedTemplateView):
