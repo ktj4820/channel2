f41279acb0e16c82f1040ca402570f5217ee031c
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2015-11-15 22:09:41 +0800

    CH-54 - Added ".mp4" extension to url pattern for VideoView

--- channel2/video/urls.py
+++ channel2/video/urls.py
@@ -5,7 +5,7 @@ from channel2.video import views
 
 urlpatterns = [
 
-    url(r'^(?P<id>\d+)/(?P<slug>[\w-]+)/$',         views.VideoView.as_view(), name='video'),
+    url(r'^(?P<id>\d+)/(?P<slug>[\w-]+).mp4$',      views.VideoView.as_view(), name='video'),
     url(r'^(?P<key>\w{64})/(?P<slug>[\w-]+).mp4$',  views.VideoLinkView.as_view(), name='video.link'),
 
 ]
